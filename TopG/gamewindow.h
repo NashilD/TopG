29e35ee611303e2aaeb34cc1223da7ec68c307be
Nashil <nashild@aucegypt.edu>
2023-11-14 18:54:48 +0200

    Adding qt files and UML diagram

---
+++ TopG/gamewindow.h
@@ -0,0 +1,29 @@
+#ifndef GAMEWINDOW_H
+#define GAMEWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class GameWindow; }
+QT_END_NAMESPACE
+
+class GameWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    GameWindow(QWidget *parent = nullptr);
+    ~GameWindow();
+    Bool Game_over;
+    int GetShots();
+    void SetShots(int);
+    int GetLevel();
+    void SetShots(int);
+private:
+    Ui::GameWindow *ui;
+    int Current_Level;
+    Target targets;
+    Obstacles obstacles;
+    int Remaing_shots;
+};
+#endif // GAMEWINDOW_H
